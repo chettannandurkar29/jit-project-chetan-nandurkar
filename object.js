6ca5486478b62fb78944126769cd749c4c12f5fe
chettannandurkar29 <chetanandurkar71@gmail.com>
2026-03-18 16:13:59 +0530

    third  commit

--- object.js
+++ object.js
@@ -1,6 +1,25 @@
+// const object = {
+//     name: "Rahul",
+//     age: "22"
+// };
+
+// object.age = 26;
+
+// console.log(object["age"]);
+// console.log(object.name);
+
 const object = {
+    name: "rahul",
+    age: 22,
+    city: "Delhi",
+    profession: "Software Engineer"
+}
+
+obj = {
     name: "Rahul",
-    age: 22
-};
-console.log(object.age);
-console.log(object.name);
+    age: 22,
+    city: "Delhi",
+    profession: "Software Engineer"
+}
+ 
+console.log(object);
